d3789d1feb80312222dffc47e12dfee319ea0af7
Linux-Maniac <151807519+Linux-Maniac@users.noreply.github.com>
2024-10-13 19:48:31 +0530

    added caching dependencies and logic, changed hibernate auto-dll to none

--- expense-tracker-backend/src/main/java/com/divyajyoti/expense_tracker_backend/ExpenseTrackerBackendApplication.java
+++ expense-tracker-backend/src/main/java/com/divyajyoti/expense_tracker_backend/ExpenseTrackerBackendApplication.java
@@ -2,8 +2,10 @@ package com.divyajyoti.expense_tracker_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ExpenseTrackerBackendApplication {
 
 	public static void main(String[] args) {
--- expense-tracker-backend/src/main/java/com/divyajyoti/expense_tracker_backend/service/CategoryService.java
+++ expense-tracker-backend/src/main/java/com/divyajyoti/expense_tracker_backend/service/CategoryService.java
@@ -5,12 +5,17 @@ import com.divyajyoti.expense_tracker_backend.model.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Getter
 @Service
 @NoArgsConstructor
@@ -24,14 +29,17 @@ public class CategoryService {
         return categoryRepository.findAll();
     }
 
+    @Cacheable(value = "category", key = "#id")
     public Optional<Category> getCategory(Integer id) {
         return categoryRepository.findById(id);
     }
 
+    @CachePut(value = "category", key = "#request.id")
     public Category createCategory(Category request) {
         return categoryRepository.save(request);
     }
 
+    @CacheEvict(value = "category", key = "#id")
     public Category updateCategory(Category request, Integer id) {
         return categoryRepository.save(request);
 
